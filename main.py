c556efdeac85448043ea66c809f735b5d9cf2d1d
tehilal <tehila.leima@huji.mail.ac.il>
2024-07-29 14:42:31 +0300

    add config file for passing parameters, and change the scripts so they will work with it

--- main.py
+++ main.py
@@ -4,22 +4,37 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import detect
 import subsToSixtyTwentyWithMask
-from pathlib import Path
+import configparser
+
+CONFIG_FILE = 'config.ini'
+
+
+def load_config(config_file):
+    # Load configuration file
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    # Retrieve paths from the configuration file
+    output_dir = config['Paths']['output_dir']
+
+    # Retrieve file names from the configuration file
+    dp_tb = config['Paths']['dependentpeptides_file']
+    pep_tb = config['Paths']['peptides_file']
+    dna_file = config['Paths']['dna_file']
+    proteins_file = config['Paths']['proteins_file']
+    return dp_tb, pep_tb, dna_file, proteins_file, output_dir
+
 
 if __name__ == '__main__':
-    #
-    slava_dir = Path(r'C:\Users\tehilal\OneDrive - weizmann.ac.il\Weizmann\Pilpelab\Collateral Vaccine Design\SixtyTwenty collection\Slava')
-    dp_tb = slava_dir / 'dependentPeptides.txt'
-    pep_tb = slava_dir / 'peptides.txt'
-    dna_file = ''
-    proteins_file = slava_dir / 'proteins.fa'
-    output_dir = ''
-
+    dp_tb, pep_tb, dna_file, proteins_file, output_dir = load_config(CONFIG_FILE)
 
     subs = detect.main(dp_tb, pep_tb, dna_file, proteins_file, output_dir)
+
+    # if dna fasta was provided and the substitutions were mapped to
     subs_codons = subs[subs.codon != 'None']
     if not subs_codons.empty:
         subsToSixtyTwentyWithMask.main(subs_codons, output_dir)
+
     plt.figure()
     sns.histplot(subs['log_intensities_ratio'])
     plt.savefig(os.path.join(output_dir, "intensity_ratio_distribution.png"))
